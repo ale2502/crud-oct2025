0c7e8b1352e0a65eae0fb9d9323b82f321168d73
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-10-18 17:50:30 -0300

    creating an array of objects for user information

--- frontend/scripts/countries.js
+++ frontend/scripts/countries.js
@@ -3,8 +3,6 @@ async function loadCountries() {
     const response = await fetch('https://restcountries.com/v3.1/all?fields=name,flags');
     const data = await response.json();
 
-    console.log(data[0]);
-
     const select = document.getElementById('nationality');
     const countries = data
       .map(c => c.name.common)
--- frontend/scripts/form-page.js
+++ frontend/scripts/form-page.js
@@ -1,5 +1,27 @@
 const form = document.getElementById('form1');
 
+const userFirstName = document.getElementById('fname');
+const userLastName = document.getElementById('lname');
+const userDateOfBirth = document.getElementById('dob');
+const userEmail = document.getElementById('email');
+const userGender = document.getElementById('gender');
+const userCountry = document.getElementById('nationality');
+
 form.addEventListener('submit', (e) => {
-  alert('xy');
+  e.preventDefault();
+  
+  let userInformation = JSON.parse(localStorage.getItem('userInformation')) || [];
+
+  const newUser = {
+    firstName: userFirstName.value,
+    lastName: userLastName.value,
+    dateOfBirth: userDateOfBirth.value,
+    email: userEmail.value,
+    gender: userGender.value,
+    country: userCountry.value
+  };
+
+  userInformation.push(newUser);
+  localStorage.setItem('userInformation', JSON.stringify(userInformation));
+  console.log(userInformation);
 });
