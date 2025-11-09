c030eb3708fd58e9e38079927a3f55e713c5996b
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-11-08 22:59:54 -0300

    playing with tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html", // all html files in the project
+    "./frontend/scripts/**/*.js" // all js files in frontend/scripts
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
